70ab0241cf4f6f4c5a10d688ee4bed3141e08a80
Matheus Barreto <matheuspbarreto@hotmail.com.br>
2024-05-15 01:10:28 -0300

    add: configuração inicial

---
+++ literalura/literalura/src/main/java/com/alura/literalura/LiteraluraApplication.java
@@ -0,0 +1,24 @@
+package com.alura.literalura;
+
+import com.alura.literalura.principal.Principal;
+import com.alura.literalura.repository.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiteraluraApplication implements CommandLineRunner {
+	@Autowired
+	private BookRepository repositorio;
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(repositorio);
+		principal.exibeMenu();
+	}
+}
